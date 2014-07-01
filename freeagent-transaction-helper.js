df9cfb3d9d5ec179938e16481521b339a08c140e
James Mead <james@floehopper.org>
2014-07-01 14:38:55 +0100

    Change Sky Digital to require an attachment

--- freeagent-transaction-helper.js
+++ freeagent-transaction-helper.js
@@ -57,7 +57,7 @@
     ['TOTAL CHARGES TO', 'HSBC - Monthly account maintenance fee', '0', 'Bank/Finance Charges', false, 'Non-EC'],
     ['SVCSAPPS_G', 'Google Apps for Business', '0', 'Computer Software', true, 'EC Services'],
     ['HARMONIA.IO', 'Harmonia - Monthly subscription', '0', 'Computer Software', true, 'Non-EC'],
-    ['SKY DIGITAL', 'Sky Digital - Broadband', '20', 'Internet & Telephone', false, 'Non-EC']
+    ['SKY DIGITAL', 'Sky Digital - Broadband', '20', 'Internet & Telephone', true, 'Non-EC']
   ]
 
   var tryToExplainTransaction = function() {
