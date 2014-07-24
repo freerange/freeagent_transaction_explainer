4f3d13dce2cee277c5d8c7bba773c44642d5f1f9
Chris Roos <chris@seagul.co.uk>
2014-07-24 18:02:10 +0100

    Display message when no attachment is required
    I think it's better to explicit state whether an attachment is required
    or not. We already had a message to explain that an attachment was
    required, and we now have one to explain when it's unnecessary.
    
    * Load jQuery and jQuery UI in jasmine.yml configuration as our
    JavaScript relies on both of them.
    
    * Stub `$.getJSON` so that we can programmatically inject the rules
    we're interested in.
    
    * Supply the `fail` function as a no-op to avoid an 'undefined
    function' error.
    
    * Use jQuery to create the HTML fixture required by the unit test. I'm
    not convinced this is the best way but it's good enough.
    
    Fixes #1

--- spec/javascripts/freeagent_transaction_helper_spec.js
+++ spec/javascripts/freeagent_transaction_helper_spec.js
@@ -1,5 +1,23 @@
 describe('freeagent-transaction-helper', function() {
-  it('foo', function() {
-    expect(true).toBe(true);
+  beforeEach(function() {
+    var fixture = $('<div>').attr('id', 'fixture').css('display', 'none')
+                  .append($('<div>').addClass('transaction unexplained').text('Pact Coffee'))
+                  .append($('<div>').attr('id', 'attachment_form')
+                    .append($('<h2>').text('Attachment')));
+
+    $(document.body).append(fixture);
+  });
+
+  it('displays a message when no attachment is required', function() {
+    var rules = [{ "textToMatch": "Pact Coffee", "description": "Pact Coffee London", "vat": "0", "category": "Sundries", "shouldHaveAttachment": false, "ecStatus": "Non-EC" }];
+    $.getJSON = function(url, callback) {
+      callback(rules);
+      return {fail: function() {}};
+    };
+
+    FreeAgentTransactionHelper('dummy-rules-url');
+
+    var attachmentHeading = $('#attachment_form h2');
+    expect(attachmentHeading.text()).toMatch(/No attachment required/);
   });
 });
