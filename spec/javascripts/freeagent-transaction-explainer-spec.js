beaef11cc6b2f9cdf984f31c9ec2879f9b97f68b
James Mead <3169+floehopper@users.noreply.github.com>
2018-06-09 11:52:09 +0100

    Update unexplained transaction page fixture and associated tests

--- spec/javascripts/freeagent-transaction-explainer-spec.js
+++ spec/javascripts/freeagent-transaction-explainer-spec.js
@@ -8,13 +8,13 @@ describe('freeagent-transaction-explainer', function() {
                   .append($('<select id="spending_category"><option>Sundries</option></select>'))
                   .append($('<input id="description"/>'))
                   .append($('<div>').attr('id', 'attachment_form')
-                    .append($('<h2>').text('Attachment')));
+                    .append($('<legend>').text('Attachment')));
 
     $(document.body).append(fixture);
   });
 
   it('displays a message when no attachment is required', function() {
-    var rules = [{ "textToMatch": "Pact Coffee", "description": "Pact Coffee London", "vat": "0%", "category": "Sundries", "shouldHaveAttachment": false, "ecStatus": "Non-EC" }];
+    var rules = [{ "textToMatch": "Pact Coffee", "description": "Pact Coffee London", "vat": "0%", "category": "Sundries", "shouldHaveAttachment": false, "ecStatus": "UK/Non-EC" }];
     $.getJSON = function(url, callback) {
       callback(rules);
       return {fail: function() {}};
@@ -22,7 +22,7 @@ describe('freeagent-transaction-explainer', function() {
 
     FreeAgentTransactionHelper('dummy-rules-url');
 
-    var attachmentHeading = $('#attachment_form h2');
-    expect(attachmentHeading.text()).toMatch(/No attachment required/);
+    var attachmentFromLegend = $('#attachment_form legend');
+    expect(attachmentFromLegend.text()).toMatch(/No attachment required/);
   });
 });
